c015ea9396805cd1e606cc32efe0806c0d66ee07
Dimitri Zhvania <dzhvania@credo.ge>
2026-05-30 11:28:12 +0400

    [Dimitri Z] HW4 GradeCalculator

---
+++ src/main/java/Homework4/GradeCalculator.java
@@ -0,0 +1,21 @@
+//@author <Dimitri Zhvania>
+package Homework4;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+
+        int month = 1;
+
+        if (month == 12) {
+            System.out.println("Grade: A");
+        } else if (month >= 9 && month <= 11) {
+            System.out.println("Grade: B");
+        } else if (month >= 6 && month <= 8) {
+            System.out.println("Grade: C");
+        } else if (month >= 3 && month <= 5) {
+            System.out.println("Grade: D");
+        } else if (month >= 1 && month <= 2) {
+            System.out.println("Grade: F");
+        }
+    }
+}
