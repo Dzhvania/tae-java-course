321ab443187bc9f04981dc38cfc911cc9851c976
Dimitri Zhvania <dzhvania@credo.ge>
2026-06-03 15:42:53 +0400

    [Dimitri Z.] HW5 - Fibonacci

---
+++ src/main/java/homework5/Fibonacci.java
@@ -0,0 +1,26 @@
+package homework5;
+
+public class Fibonacci {
+
+    public static long fib(int n) {
+
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        long prev1 = 1;
+        long prev2 = 0;
+        long current = 0;
+
+        for (int i = 2; i <= n; i++) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return current;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(26));
+    }
+}
