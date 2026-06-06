61cdbb7f82fa1893092d1f274e5bd26186e8dfac
Dimitri Zhvania <dzhvania@credo.ge>
2026-06-06 12:28:18 +0400

    [Dimitri Z] HW6 - Car

---
+++ src/main/java/homework6/Car.java
@@ -0,0 +1,21 @@
+//@Author Dimitri Zhvania
+package homework6;
+
+public class Car extends Vehicle {
+
+    int doors;
+
+    public Car(String brand, int year, int doors) {
+        super(brand, year);
+        this.doors = doors;
+    }
+
+    @Override
+    public void info() {
+        System.out.println(brand + " " + year + ", " + doors + " doors");
+    }
+
+    public static void main(String[] args) {
+        new Car("Toyota", 2020, 4).info();
+    }
+}
