bb8aa48982d515bf4477dc76e2229c6401cf1145
Dimitri Zhvania <dzhvania@credo.ge>
2026-06-10 14:22:44 +0400

    [Dimitri Z.] HW7 - Triangle

---
+++ src/main/java/Homework7/Triangle.java
@@ -0,0 +1,10 @@
+//@Author Dimitri Zhvania
+package Homework7;
+
+public class Triangle implements Drawable {
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing Triangle");
+    }
+}
